19dfdb404985cba9d12704cc944864f0ea12b53b
PiusAsh <Pius1ash@gmail.com>
2022-04-13 09:41:22 +0100

    Initial Commit

---
+++ testing.js
@@ -0,0 +1,22 @@
+var toggle = document.getElementById("toggle");
+var content = document.getElementById("content");
+
+toggle.addEventListener("click", function () {
+    content.classList.toggle("appear");
+}, false);
+
+
+// var toggle = document.getElementById("skills");
+// var content = document.getElementById("skillContent");
+
+// toggle.addEventListener("click", function () {
+//     content.classList.toggle("appear");
+// }, false);
+
+
+// var toggle1 = document.getElementById("toggle1");
+// var content = document.getElementById("content1");
+
+// toggle.addEventListener("click", function () {
+//     content.classList.toggle("appear");
+// }, false);
